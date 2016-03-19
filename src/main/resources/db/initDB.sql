0dc8b60a856acd8331baabe8f460569ba7bea226
Vladimir Sentsov <vladimirsencov@yandex.ru>
2016-03-19 13:38:36 +0300

    start_of_HW03

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -18,8 +18,10 @@ CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
+
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+
 );
