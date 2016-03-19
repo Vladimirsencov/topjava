f74887cb106845a7194861fb6deccdee93bfec9a
Vladimir Sentsov <vladimirsencov@yandex.ru>
2016-03-19 18:04:55 +0300

    start_of_HW03

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -9,8 +9,8 @@ import ru.javawebinar.topjava.web.user.AdminRestController;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * User: gkislin
@@ -20,7 +20,7 @@ public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml")) {
-            System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
+            Stream.of(appCtx.getBeanDefinitionNames()).forEach(System.out::println);
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             System.out.println(adminUserController.create(UserTestData.USER));
             System.out.println();
