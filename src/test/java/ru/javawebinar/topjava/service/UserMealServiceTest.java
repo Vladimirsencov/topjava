ac7e72749c4bf8d412e4bffe3528129d9500f40a
Vladimir Sentsov <vladimirsencov@yandex.ru>
2016-03-22 23:02:43 +0300

    realization testUserMealService

---
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+
+/**
+ * Created by Vladimir_Sentso on 22.03.2016.
+ */
+public class UserMealServiceTest {
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDates() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDateTimes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+}
