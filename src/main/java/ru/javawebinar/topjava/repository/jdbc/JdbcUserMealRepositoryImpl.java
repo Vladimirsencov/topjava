e7c1387ae26af0ca68e3b8a1e8a34db51428b235
Vladimir Sentsov <vladimirsencov@yandex.ru>
2016-03-23 16:36:55 +0300

    Spring realization the end

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserMealRepositoryImpl.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -28,8 +29,11 @@ public class JdbcUserMealRepositoryImpl implements UserMealRepository {
             = BeanPropertyRowMapper.newInstance(UserMeal.class);
 
     private final SimpleJdbcInsert userMealInsert;
+
     @Autowired
+    @Qualifier("jdbcTemplate")
     private JdbcTemplate jdbcTemplate;
+
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
--- src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
@@ -1,9 +1,12 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.util.DbPopulator;
 
 /**
  * Created by Vladimir_Sentso on 22.03.2016.
@@ -14,6 +17,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserMealServiceTest {
+
+    @Autowired
+    protected UserMealService userMealService;
+
+    @Autowired
+    private DbPopulator dbPopulator;
+
+    @Before
+    public void setUp() throws Exception {
+        dbPopulator.execute();
+    }
+
 
     @Test
     public void testGet() throws Exception {
