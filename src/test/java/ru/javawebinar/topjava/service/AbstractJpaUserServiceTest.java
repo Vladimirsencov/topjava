189db5a367309807d621d6184b70dcf0d288821c
VladimirSentcovForJavarushTestTask <vladimirsencov@gmail.ru>
2016-04-11 11:19:26 +0300

    HW06

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.topjava.service;
+
+import org.aspectj.lang.annotation.SuppressAjWarnings;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+/**
+ * Created by Sentsov on 11.04.2016.
+ */
+public class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
+    @SuppressAjWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
